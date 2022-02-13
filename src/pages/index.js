df48aebf405e8d4fba372d6f654b2d28d90507ec
ggottemo <ggottemo717@gmail.com>
2022-02-13 16:54:47 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Imports
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 //
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following hte Gatsby Tutorial. </p>
+      <StaticImage 
+      alt="Night Sky"
+      src="../images/blue_night.png"
+      />
     </Layout>
   )
 }
