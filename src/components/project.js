c7394faf4ff2d8b642ac317088c1806a5894f2c1
ggottemo <ggottemo717@gmail.com>
2022-02-16 23:06:09 -0500

    add other projects, fix template for generating tag divs, fix url bug

--- src/components/project.js
+++ src/components/project.js
@@ -4,13 +4,20 @@ import * as React from 'react';
 
 //create project component
 const Project = ( props ) => {
+ 
     return (
         <div className="project">
             <h3>{ props.title }</h3>
-            <div className="project-tags"> { props.tags }</div>
+            {/* generate divs with appropriate class for each tag */}
+            {/* inteded to use for styling later */}
+            <div className="project-tags"> { props.tags.map((tag) => (
+                <div className={'tag-' + tag}>{tag}</div>
+    )) }</div>
             <p className="project-description">{ props.description }</p>
             <a href={ props.github }>Github</a> <br></br>
-            <a href={ props.deployed }>Deployed</a>
+            {/* Check to see if there is a deploy link */}
+            { props.deployed ? <a href={ props.deployed }>Deployed</a> : null }
+
         </div>
     )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,11 +6,27 @@ import Project from '../components/project'
 //prop variables
 const hulu = {
     title : 'Hulu 2.0 Clone',
-    tags : ['Tailwind', 'React',],
+    tags : ['Tailwind', 'React', 'APIs'],
     description : 'A clone of Hulu 2.0 built with React and Tailwind CSS.',
-    github : 'https://github.com/ggottemo/hulu-2,',
+    github : 'https://github.com/ggottemo/hulu-2',
     deployed : 'https://hulu-2-pdeii248i-ggottemo.vercel.app/',
 }
+
+const etch_js = {
+    title : 'Etch.js',
+    tags : ['JavaScript'],
+    description : 'A simple drawing application built with JavaScript.',
+    github : 'https://github.com/ggottemo/etch-js',
+    deployed : 'https://etch-js-ggottemo.vercel.app/',
+}
+
+const python_scripts = {
+    title : 'Python Scripts',
+    tags : ['Python'],
+    description : 'A collection of Python scripts I used to automate my day-to-day tasks.',
+    github : 'https://github.com/ggottemo/python-repo',
+}
+
 
 
 const ProjectsPage = () => {
@@ -18,6 +34,8 @@ const ProjectsPage = () => {
     <Layout pageTitle="Projects">
       <p>This is the projects page</p>
         <Project {...hulu} />
+        <Project {...etch_js} />
+        <Project {...python_scripts} />
     </Layout>
   )
 }
