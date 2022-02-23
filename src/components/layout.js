5abddb90e94c6b7e5af34839bef97e41e705052a
ggottemo <ggottemo717@gmail.com>
2022-02-23 00:40:53 -0500

    style changes, removed unused component

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,8 +7,7 @@ import {
     navLinkItem,
     navLinkText,
     siteTitle,
-    hamb,
-    navctive,
+
     } from '../styles/layout.module.css'
 import Socials from './socials'
 import cv from "./cv.pdf"
@@ -28,7 +27,7 @@ const Layout = ({  pageTitle, children }) => {
         <div className={container}>
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
             <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-            <a href="javascript:void(0);" class="hamb" onClick="navFunction()">&#9776;</a>
+            
             <nav>
                 <ul className={navLinks}>
                         <Link to="/" className={navLinkText}>
